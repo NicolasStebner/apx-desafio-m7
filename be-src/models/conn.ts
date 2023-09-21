bcdb80d8eddd83aac82d58eeb84f32f4d9da0403
Niho <101224550+NicolasStebner@users.noreply.github.com>
2023-09-21 14:50:18 -0300

    Delete conn

--- be-src/models/conn.ts
+++
@@ -1,7 +0,0 @@
-import { Sequelize, DataTypes, Model } from "sequelize";
-
-const sequelize = new Sequelize();
-/* "postgres://vhvdmels:dqoL2bOGDofimblYxckxgMVzCoipjNTu@motty.db.elephantsql.com/vhvdmels" */
-//generar un postgre y conectarlo
-
-export { sequelize };
