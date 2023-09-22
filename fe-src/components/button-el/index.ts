0f128efe8bd36366d3ec637c7f06bf70e0bf186e
Nicolas Stebner <nicolas_stebner@hotmail.com>
2023-09-22 20:09:41 -0300

    first button-el model

---
+++ fe-src/components/button-el/index.ts
@@ -0,0 +1,35 @@
+export function init() {
+	class Button extends HTMLElement {
+		constructor() {
+			super();
+			this.refactor();
+		}
+		refactor() {
+			const textoDeEntrada = this.getAttribute("label");
+			const color = this.getAttribute("color");
+			var style = document.createElement("style");
+			style.textContent = `
+            :host{
+                display:block;
+            }
+            .boton{
+                border-radius: 10px;
+                padding: 14px 0 17px;
+                text-align:center;
+                color:white;
+                font-size:16px;
+            }
+            `;
+			var shadow = this.attachShadow({ mode: "open" });
+			shadow.appendChild(style);
+
+			var div = document.createElement("div");
+			div.classList.add("boton");
+			div.textContent = textoDeEntrada;
+			div.style.backgroundColor = color;
+
+			shadow.appendChild(div);
+		}
+	}
+	customElements.define("button-el", Button);
+}
