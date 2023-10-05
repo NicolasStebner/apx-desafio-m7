e9777125c9fa8ad67fc88304918afb07dfa818d8
Nicolas Stebner <nicolas_stebner@hotmail.com>
2023-10-05 15:45:10 -0300

    implementation of signup

--- fe-src/pages/signup/index.ts
+++ fe-src/pages/signup/index.ts
@@ -1,3 +1,4 @@
+import { state } from "../../state";
 export function initSignUp(params) {
 	const div = document.createElement("div");
 	div.classList.add("contenedor");
@@ -5,14 +6,32 @@ export function initSignUp(params) {
 	<header-el></header-el>
     <main class="main">
 	    <title-el class="title centrado" label="Registrarse"></title-el>
-	    <h4>Ingresá los siguientes datos para realizar el registro</h4>
-		<input-el label="EMAIL" placeholder=""></input-el>
-		<input-el label="CONTRASEÑA" placeholder=""></input-el>
-		<input-el label="CONFIRMAR CONTRASEÑA" placeholder=""></input-el>
-        <h5>¿Ya tenes una cuenta? Iniciar Sesión</h5>
+	    <h4 class="centrado">Ingresá los siguientes datos para realizar el registro</h4>
+		<input-el class="input" label="EMAIL" placeholder=""></input-el>
+		<input-el class="input" label="CONTRASEÑA" type="password" placeholder=""></input-el>
+		<input-el class="input" label="CONFIRMAR CONTRASEÑA" type="password" placeholder=""></input-el>
+        <h5 class="centrado">¿Ya tenes una cuenta? <a href="./login">Iniciar Sesión</a></h5>
         <button-el class="button" color="#5A8FEC" label="Siguiente"></button-el>
     </main>
     `;
+
+	const buttonEl = div.querySelector(".button");
+	buttonEl.addEventListener("click", async () => {
+		const inputEls = div.querySelectorAll(".input");
+		const email = inputEls[0];
+		const passFirst = inputEls[1];
+		const passSecond = inputEls[2];
+		/*  */
+		const emailValue = email.shadowRoot.querySelector(".input").value;
+		const passFirstValue = passFirst.shadowRoot.querySelector(".input").value;
+		const passSecondValue = passSecond.shadowRoot.querySelector(".input").value;
+		if (passFirstValue == passSecondValue) {
+			await state.signUp(emailValue, passFirstValue);
+			params.goTo("/home");
+		} else {
+			alert("Las contraseñas no coinciden");
+		}
+	});
 
 	return div;
 }
