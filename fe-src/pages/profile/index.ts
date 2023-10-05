6b975f0b624a85e0d73de375d19bb0e91185d872
Nicolas Stebner <nicolas_stebner@hotmail.com>
2023-10-05 15:44:48 -0300

    show email of user on the screen

--- fe-src/pages/profile/index.ts
+++ fe-src/pages/profile/index.ts
@@ -1,5 +1,7 @@
-export function initProfile(params) {
+import { state } from "../../state";
+export async function initProfile(params) {
 	const div = document.createElement("div");
+	const email = await state.getEmail();
 	div.classList.add("contenedor");
 	div.innerHTML = `
 	<header-el></header-el>
@@ -7,20 +9,21 @@ export function initProfile(params) {
 	    <title-el class="title centrado"label="Mis datos"></title-el>
         <button-el class="button datos" color="#5A8FEC" label="Modificar datos personales"></button-el>
         <button-el class="button password" color="#5A8FEC" label="Modificar contraseña"></button-el>
-        <h5>EMAIL_USER@HOTMAIL.COM</h5>
-        <p>Cerrar Sesión</p>
+        <h5 class="centrado">${email}</h5>
+        <p class="centrado"><a class="logout" href="./login">Cerrar Sesión</a></p>
     </main>
     `;
 	const datosButton = div.querySelector(".datos");
-
+	const passwordButton = div.querySelector(".password");
+	const logoutEl = div.querySelector(".logout");
 	datosButton.addEventListener("click", () => {
 		params.goTo("/profile-datos-personales");
 	});
-	const passwordButton = div.querySelector(".password");
 	passwordButton.addEventListener("click", () => {
-		console.log("click para password");
 		params.goTo("/profile-cambiar-contrasenia");
 	});
-
+	logoutEl.addEventListener("click", async () => {
+		await state.logOut();
+	});
 	return div;
 }
