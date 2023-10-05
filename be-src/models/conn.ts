877b671bc30d022a7ca471d0bdd0841d041d7964
Nicolas Stebner <nicolas_stebner@hotmail.com>
2023-10-05 15:39:04 -0300

    new conn sequelize

--- be-src/models/conn.ts
+++ be-src/models/conn.ts
@@ -4,16 +4,6 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.POSTGRES_URL);
 
-/* async function connSeq() {
-	try {
-		await sequelize.authenticate();
-		console.log("Connection has been established successfully");
-	} catch (error) {
-		console.log("Unable to connect to the database", error);
-	}
-}
-connSeq(); */
-
 /* sequelize.sync({ force: true }); */
 
 export { sequelize };
