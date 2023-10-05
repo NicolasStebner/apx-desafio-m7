b2643b08c0181340d96975d9b541e630cae06a20
Nicolas Stebner <nicolas_stebner@hotmail.com>
2023-10-05 15:45:42 -0300

    change password implementation

--- fe-src/pages/profile-contrasenia/index.ts
+++ fe-src/pages/profile-contrasenia/index.ts
@@ -1,3 +1,5 @@
+import { state } from "../../state";
+
 export function initProfileCambiarContrasenia(params) {
 	const div = document.createElement("div");
 	div.classList.add("contenedor");
@@ -5,11 +7,30 @@ export function initProfileCambiarContrasenia(params) {
 	<header-el></header-el>
     <main class="main">
 	    <title-el class="title centrado" label="Contraseña"></title-el>
-        <input-el label="CONTRASEÑA" placeholder=""></input-el>
-        <input-el label="CONFIRMAR CONTRASEÑA" placeholder=""></input-el>
+        <input-el class="input" label="CONTRASEÑA" type="password" placeholder=""></input-el>
+        <input-el class="input" label="CONFIRMAR CONTRASEÑA" type="password" placeholder=""></input-el>
         <button-el class="button" color="#5A8FEC" label="Guardar"></button-el>
     </main>
     `;
-
+	const buttonEl = div.querySelector(".button");
+	buttonEl.addEventListener("click", async () => {
+		const [firstInput, secondIn] = div.querySelectorAll(".input");
+		const firstInputValue = firstInput.shadowRoot.querySelector(".input").value;
+		const secondInputValue = secondIn.shadowRoot.querySelector(".input").value;
+		if (firstInputValue == secondInputValue) {
+			const rta = await state.cambiarContrasenia(firstInputValue);
+			if (rta["message"] == "password updated") {
+				alert("contraseña cambiada");
+			} else if (rta["message"] == "user not found") {
+				alert("el usuario no fue encontrado, debe registrarse");
+				params.goTo("/signup");
+			} else {
+				alert("no iniciaste sesion");
+				params.goTo("/login");
+			}
+		} else {
+			alert("las contraseñas deben coincidir");
+		}
+	});
 	return div;
 }
