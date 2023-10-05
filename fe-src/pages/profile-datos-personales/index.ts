a597d092ea63b7980b62b1af39f209136939bb1d
Nicolas Stebner <nicolas_stebner@hotmail.com>
2023-10-05 15:46:12 -0300

    setting name and location of user

--- fe-src/pages/profile-datos-personales/index.ts
+++ fe-src/pages/profile-datos-personales/index.ts
@@ -1,3 +1,5 @@
+import { state } from "../../state";
+
 export function initProfileDatosPersonales(params) {
 	const div = document.createElement("div");
 	div.classList.add("contenedor");
@@ -5,11 +7,25 @@ export function initProfileDatosPersonales(params) {
 	<header-el></header-el>
     <main class="main">
 	    <title-el class="title centrado" label="Datos Personales"></title-el>
-        <input-el label="Nombre" placeholder=""></input-el>
-        <input-el label="Localidad" placeholder=""></input-el>
+        <input-el class="input" label="Nombre" placeholder=""></input-el>
+        <input-el class="input" label="Localidad" placeholder=""></input-el>
         <button-el class="button" color="#5A8FEC" label="Guardar"></button-el>
     </main>
     `;
+	/* En caso de completar los datos, guardarlos en la db */
+	const button = div.querySelector(".button");
+	button.addEventListener("click", async () => {
+		const nombreCont = div.querySelectorAll(".input")[0];
+		const nombreValue = nombreCont.shadowRoot.querySelector(".input").value;
+		const localidadCont = div.querySelectorAll(".input")[1];
+		const localValue = localidadCont.shadowRoot.querySelector(".input").value;
+		const rta = await state.setNombreAndLocalidad(nombreValue, localValue);
+		if (!rta["message"]) {
+			alert("la localidad y el nombre fueron guardados");
+		} else {
+			alert("algo falló, intente más tarde");
+		}
+	});
 
 	return div;
 }
