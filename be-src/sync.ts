9269a078733ebd03a99216499dd301f1cd5b8403
Nicolas Stebner <nicolas_stebner@hotmail.com>
2023-12-20 22:09:21 -0300

    sync-sequelize

--- be-src/sync.ts
+++ be-src/sync.ts
@@ -1,7 +1,13 @@
 import { User, Mascota, Auth } from "./models";
+import { sequelize } from "./models/conn";
 User.sequelize.sync({ force: true }).then((res) => {
 	console.log(res);
 });
 Auth.sequelize.sync({ force: true }).then((res) => {
 	console.log(res);
 });
+Mascota.sequelize.sync({ force: true }).then((res) => {
+	console.log(res);
+});
+
+// sequelize.sync({force:true})
