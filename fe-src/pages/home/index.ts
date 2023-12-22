4d8fc4a750f2203334fe34278afa77f04232dac3
Nicolas Stebner <nicolas_stebner@hotmail.com>
2023-12-21 23:57:25 -0300

    new-redirects

--- fe-src/pages/home/index.ts
+++ fe-src/pages/home/index.ts
@@ -8,12 +8,32 @@ export async function initPageWelcome(params) {
 		<title-el class="title centrado" color="#EB6372" label="Pet Finder App"></title-el>
 		<subtitle-el class="subtitle centrado" label="Encontrá y reportá mascotas perdidas cerca de tu ubicación"></subtitle-el>
 		<button-el class="button ubicacion" color="#5A8FEC" label="Dar mi ubicacion actual"></button-el>
-		<button-el class="button" color="#00A884"label="¿Cómo funciona Pet Finder?"></button-el>
+		<button-el class="button comoFunciona" color="#00A884"label="¿Cómo funciona Pet Finder?"></button-el>
 	</main>
     `;
 	const buttonUbicacionEl = div.querySelector(".ubicacion");
 	buttonUbicacionEl.addEventListener("click", () => {
-		params.goTo("/home-mascotas");
+		console.log(navigator.geolocation)
+		if (navigator.geolocation) {
+			// Obtiene la ubicación actual del usuario
+			navigator.geolocation.getCurrentPosition(function (position) {
+				const latitud = position.coords.latitude;
+				const longitud = position.coords.longitude;
+				// Ahora puedes usar estas coordenadas con Mapbox
+				sessionStorage.setItem("lat_user",latitud.toString())
+				sessionStorage.setItem("lng_user",longitud.toString())
+				// Aquí puedes agregar tu lógica para utilizar Mapbox con las coordenadas obtenidas
+				params.goTo("/mascota-cerca");
+			}, function (error) {
+				console.error(`Error al obtener la ubicación: ${error.message}`);
+			});
+		}else{
+			alert("Tu navegador no soporta esta versión, intentelo más tarde")
+		}
 	});
+	const buttonComoFuncionaEl = div.querySelector(".comoFunciona");
+	buttonComoFuncionaEl.addEventListener("click",()=>{
+		params.goTo("/como-funciona-pet-finder")
+	})
 	return div;
 }
